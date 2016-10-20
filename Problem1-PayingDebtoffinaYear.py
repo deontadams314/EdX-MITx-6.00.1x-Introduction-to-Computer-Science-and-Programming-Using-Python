563431c5e98e21ad2299046243c2e0dc02f9a62f
deontadams314 <deontadams314@gmail.com>
2016-10-20 02:40:10 -0400

    Update Problem1-PayingDebtoffinaYear.py

--- Problem1-PayingDebtoffinaYear.py
+++ Problem1-PayingDebtoffinaYear.py
@@ -4,7 +4,6 @@ def debtProb(balance, annualInterestRate, monthlyPaymentRate):
         min_monthly_pay = monthlyPaymentRate * balance
         monthly_unpaid_bal = balance - min_monthly_pay
         updated_balance = round(monthly_unpaid_bal + (monthly_interest * monthly_unpaid_bal), 2)
-        #month += 1
         balance =  updated_balance
     print ("Remaining balance: " + str(balance))
 
